7cd2a8b58005043b796d43c086057db5d3ff7764
moenguyenx <qminh1908@gmail.com>
2023-11-23 16:30:25 +0700

    restructured-require certain change in js & template

---
+++ cafe/routes.py
@@ -0,0 +1,14 @@
+from cafe import app, menu
+from flask import render_template, request
+
+
+@app.route("/", methods=["GET", "POST"])
+def order():
+    if request.method == "GET":
+        drink_list = menu.find()
+        return render_template("index.html", drink_list=drink_list)
+
+    if request.method == "POST":
+        pass
+
+
