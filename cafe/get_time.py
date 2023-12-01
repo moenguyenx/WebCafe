90ba36f3d116c2f03ffc74b449a0bf6a403d913d
moenguyenx <qminh1908@gmail.com>
2023-12-02 00:36:55 +0700

    added all templates for admin and staff, prepare for upcoming features

--- cafe/get_time.py
+++ cafe/get_time.py
@@ -2,6 +2,9 @@ from datetime import datetime
 
 
 def get_date():
+    """
+    :return: Date as dd-mm-yyyy
+    """
     current = datetime.now()
     day = current.strftime("%d")
     month = current.strftime("%m")
@@ -13,10 +16,13 @@ def get_date():
 
 
 def get_current_time():
+    """
+    :return: Current time as Hour:Min (0-23h)
+    """
     current = datetime.now()
     hour = current.strftime("%H")  # 0-23h
     minute = current.strftime("%M")
 
-    current_time = f"{hour}{minute}"
+    current_time = f"{hour}:{minute}"
 
     return f"{current_time}"
